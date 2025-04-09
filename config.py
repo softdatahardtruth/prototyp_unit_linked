4c9f2742255819aa5b443ac0a63f0acf137c4895
softdatahardtruth <jessica.noller@outlook.de>
2025-04-09 08:42:44 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def initialize_page():
+    st.set_page_config(page_title="Allianz VitaVerde Simulator", layout="wide")
+    st.title("Allianz VitaVerde - Insurance & Investment Simulator")
